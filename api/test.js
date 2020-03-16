af4d568ab46bbfcf7e9ca20b6064cb6b3972d3ee
oops1did <62184680+oops1did@users.noreply.github.com>
2020-03-16 21:41:59 +0000

    Update test.js

--- api/test.js
+++ api/test.js
@@ -4,7 +4,7 @@ var os = require('os');
 var querystring = require('querystring');
 
 module.exports = function (req, res) {
-    if (req.method = 'POST') {
+    if (req.method === 'PUT') {
         res.end('from post');
     } else {
         res.end(req.url);
