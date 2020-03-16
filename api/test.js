8719bbb920b39bd033333be0b243c331c398e0c6
oops1did <62184680+oops1did@users.noreply.github.com>
2020-03-16 21:54:04 +0000

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,17 +1,29 @@
 
-var fs = require('fs');
-var os = require('os');
-var querystring = require('querystring');
+var fs = require('fs')
+var os = require('os')
+var crypto = require('crypto')
+var querystring = require('querystring')
+
+var password_hash = 'fdea8354e753932a0cd5d6b31c2cfc01f5c504ed589aba7550a5c32ba970a84f'
+
+function hash256 (thing) {
+    hash.update(thing)
+    return hash.digest('hex')
+}
 
 module.exports = function (req, res) {
     if (req.method === 'POST') {
         var incoming = ''
         req.on('data', function (d) { incoming += d.toString() })
         req.on('end', function () {
-            var t = querystring.parse(incoming);
-            res.end('from post'+JSON.stringify(t));
+            var t = querystring.parse(incoming)
+            if (t.hasOwnProperty('password') && hash256(t.password) === password_hash) {
+                res.end('from post'+JSON.stringify(t))
+            } else {
+                res.end('access denied')
+            }
         })
     } else {
-        res.end(req.url);
+        res.end(req.url)
     }
 }
