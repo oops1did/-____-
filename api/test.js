b9bbfff90f58d980701e9001446179a1db798580
oops1did <62184680+oops1did@users.noreply.github.com>
2020-03-16 22:13:41 +0000

    Update test.js

--- api/test.js
+++ api/test.js
@@ -5,7 +5,7 @@ var crypto = require('crypto')
 var querystring = require('querystring')
 var hash = crypto.createHash('sha256')
 
-var password_hash = 'ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f'
+var password_hash = 'fdea8354e753932a0cd5d6b31c2cfc01f5c504ed589aba7550a5c32ba970a84f'
 
 function hash256 (thing) {
     hash.update(thing)
