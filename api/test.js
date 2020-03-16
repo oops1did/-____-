3b9979666d92b01e28ce29666372a8df925397de
oops1did <62184680+oops1did@users.noreply.github.com>
2020-03-16 15:18:26 +0000

    Update test.js

--- api/test.js
+++ api/test.js
@@ -5,7 +5,7 @@ var os = require('os');
 module.exports = function (req, res) {
   var tmpdir = os.tmpdir();
   var dir = fs.readdirSync(tmpdir)
-  fs.writeFileSync(tmpdir+'/test.txt', 'testing123');
+  fs.writeFileSync(tmpdir+'/test.txt', new Date().toISOString());
   var file = fs.readFileSync(tmpdir+'/test.txt')
   res.end('.... '+JSON.stringify(dir)+' '+file.toString())
 }
