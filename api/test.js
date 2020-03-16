73c87c0896da691c11311372d93649bdfeb130f9
oops1did <62184680+oops1did@users.noreply.github.com>
2020-03-16 21:43:44 +0000

    Update test.js

--- api/test.js
+++ api/test.js
@@ -5,7 +5,11 @@ var querystring = require('querystring');
 
 module.exports = function (req, res) {
     if (req.method === 'PUT') {
-        res.end('from post');
+        var incoming = ''
+        req.on('data', function (d) { incoming += d.toString() })
+        req.on('end', function () {
+            res.end('from post'+incoming);
+        })
     } else {
         res.end(req.url);
     }
